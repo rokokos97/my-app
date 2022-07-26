a9b11f4183c99a336b632ffc4f23856a8fbacebf
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-07-26 20:34:20 +0200

    initial countersList before HW

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css"
 import CountersList from "./components/countersList";
 
 const App=()=>{
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,40 +1,52 @@
-import React,{useState} from "react";
-
+import React from "react";
 
 const Counter=(props)=>{
-    const [value,setValue] = useState(props.value);
-    const formatCount=()=>{
-        return value===0? "empty" :value
-    };
+    console.log(props);
+    // const [value,setValue]=useState(props.value)
+    const {value}=props
 
-    const getBagesClasses=()=> {
-        let classes = "badge m-2 ";
-        classes += value === 0 ? "bg-warning" : "bg-primary";
+    const formatValue =()=>{
+        return value === 0 ? "empty" : value
+    };
+    const getBadgeClasses =()=>{
+        let  classes ="bg m-2 btn-sm ";
+        classes += value===0 ? "bg-warning" : "bg-primary"
         return classes
     };
 
-    const handleDicriment = () =>{
-        setValue((prevState)=>prevState-1)
+    const handelIncrement =()=>{
+        // setValue(value+1)
+        console.log("handelIncrement");
     };
 
-    const handleIncriment = () =>{
-        setValue((prevState)=>prevState+1);
+    const handelDecrement=()=>{
+        // setValue(value-1)
+        console.log("handelDecrement");
     };
 
-    return <div>
+return <div>
         <span>{props.name}</span>
-        <span className={getBagesClasses()}>{formatCount()}</span>
+        <span
+            className= {getBadgeClasses()}>{formatValue()}
+        </span>
         <button
-            className="btn btn-primary btn-sm m-2"
-            onClick={handleDicriment}>
+            className="btn bg-primary btn-sm m-2"
+            onClick={handelIncrement}
+        >
+            +
+        </button>
+        <button
+            className="btn bg-primary btn-sm m-2"
+            onClick={handelDecrement}
+        >
             -
         </button>
         <button
-            className="btn btn-primary btn-sm  m-2"
-            onClick={handleIncriment}>
-            +
+            className="btn btn-danger"
+            onClick={()=>props.onDelete(props.name)}
+        >
+            Delete
         </button>
-        <button className="btn btn-danger btn-sm m-2" onClick={()=>props.onDelete(props.id)}>Delete</button>
     </div>
 };
 
